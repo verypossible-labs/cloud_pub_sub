6f93f6fef4f98f609c2d9f998d14f6ac9094aff9
Alex McLain <alex@alexmclain.com>
2020-09-04 10:48:34 -0700

    Fix TCP adapter connection opts.

--- lib/cloud_pub_sub/adapters/tortoise/tcp.ex
+++ lib/cloud_pub_sub/adapters/tortoise/tcp.ex
@@ -9,7 +9,20 @@ defmodule CloudPubSub.Adapters.Tortoise.TCP do
          host: Keyword.fetch!(opts, :host)
        ]}
 
-    CloudPubSub.Adapters.Tortoise.tortoise_connect(opts[:client_id], opts[:subscriptions], server)
+    connection_opts = %{
+      cloud_provider: opts[:cloud_provider],
+      client_id: opts[:client_id],
+      subscriptions: opts[:subscriptions],
+      server: server
+    }
+
+    connection_opts =
+      case opts[:cloud_provider] do
+        :aws -> connection_opts
+        :gcp -> Map.put(connection_opts, :password, opts[:password])
+      end
+
+    CloudPubSub.Adapters.Tortoise.tortoise_connect(connection_opts)
     {:ok, %{client_id: opts[:client_id]}}
   end
 end
